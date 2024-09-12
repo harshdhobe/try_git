8d978a040b7f48be8410bd2320f28240e0f207dc
harsh dhobe <harshdhobe123@gmail.com>
2024-09-12 21:17:16 +0530

    craeted subtract function

--- add.js
+++ add.js
@@ -5,3 +5,14 @@ function add(num1, num2) {
 }
 
 console.log(add(1, 4));
+
+//subtract function
+
+
+function subtract(num1, num2) {
+    return num1 - num2;
+    
+}
+
+console.log(subtract(1, 4));
+
